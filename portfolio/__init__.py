71a6c5ccb2e2a0072603f3f67875cee90ad6e635
Michelle Rogers <74469651+micheknows@users.noreply.github.com>
2023-02-19 09:02:08 -0700

    Added a webhook route for automatic update with GitHub

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -34,8 +34,8 @@ def create_app():
 
     from .views import views
     from .auth import auth
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     from .models import User
 

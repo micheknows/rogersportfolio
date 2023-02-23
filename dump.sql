29117dc4f392548630305120071e470ec782000d
Michelle Rogers <74469651+micheknows@users.noreply.github.com>
2023-02-22 18:01:28 -0700

    Adding abilitiy to create a new user

--- dump.sql
+++ dump.sql
@@ -31,7 +31,7 @@ CREATE TABLE `items` (
   `demo_link` varchar(255) DEFAULT NULL,
   `github_link` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
